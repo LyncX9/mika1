79367924c3088e706152f2be6e0743829f4cd4a4
LyncX9 <105217979+LyncX9@users.noreply.github.com>
2025-11-03 02:30:26 +0700

    Create fake_audioop.py

---
+++ fake_audioop.py
@@ -0,0 +1,17 @@
+# fake_audioop.py
+# Placeholder agar discord.py tidak error di Python 3.13
+def add(*args, **kwargs): return b''
+def mul(*args, **kwargs): return b''
+def bias(*args, **kwargs): return b''
+def getsample(*args, **kwargs): return 0
+def avg(*args, **kwargs): return 0
+def max(*args, **kwargs): return 0
+def rms(*args, **kwargs): return 0
+def reverse(*args, **kwargs): return b''
+def tomono(*args, **kwargs): return b''
+def tostereo(*args, **kwargs): return b''
+def lin2lin(*args, **kwargs): return b''
+def lin2ulaw(*args, **kwargs): return b''
+def ulaw2lin(*args, **kwargs): return b''
+def lin2adpcm(*args, **kwargs): return (b'', 0)
+def adpcm2lin(*args, **kwargs): return (b'', 0)
